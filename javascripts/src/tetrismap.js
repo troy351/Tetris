468bb87fcc08914b68d5d4b4b266752eda327100
troy <914053923@qq.com>
2016-09-05 23:56:32 +0800

    add map move/transform judge and set

--- javascripts/src/tetrismap.js
+++ javascripts/src/tetrismap.js
@@ -1,38 +1,139 @@
 import Tetromino from 'javascripts/tetromino';
 
 export default class TetrisMap {
-    constructor(tetrominoSize, ctx) {
-        this.tetrominoSize = tetrominoSize;
+    constructor(config, ctx) {
+        this.config = config;
         this.ctx = ctx;
 
         // init map
         this.map = [];
-        for (let i = 0; i < 20; i++) {
+        for (let i = 0; i < this.config.rows; i++) {
             const row = [];
-            for (let j = 0; j < 10; j++) {
+            for (let j = 0; j < this.config.columns; j++) {
                 row.push('');
             }
             this.map.push(row);
         }
     }
 
-    _drawBackground() {
+    drawBackground() {
         const ctx = this.ctx;
-        const size = this.tetrominoSize;
+        const size = this.config.tetrominoSize;
+        const rows = this.config.rows;
+        const columns = this.config.columns;
 
         // draw a gradient background color
-        const lineGradient = this.ctx.createLinearGradient(0, 0, size * 10, size * 20);
-        lineGradient.addColorStop(0, '#040a1b');
-        lineGradient.addColorStop(1, '#082877');
+        const lineGradient = ctx.createLinearGradient(0, 0, size * columns, size * rows);
+        lineGradient.addColorStop(0, '#082877');
+        lineGradient.addColorStop(1, '#040a1b');
         ctx.fillStyle = lineGradient;
-        ctx.fillRect(0, 0, size * 10, size * 20);
+        ctx.fillRect(0, 0, size * columns, size * rows);
 
         // draw a white grid
         ctx.strokeStyle = 'white';
-        for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < 20; j++) {
+        for (let i = 0; i < columns; i++) {
+            for (let j = 0; j < rows; j++) {
                 ctx.strokeRect(size * i, size * j, size, size);
             }
         }
+    }
+
+    draw() {
+        const size = this.config.tetrominoSize;
+        const rows = this.config.rows;
+        const columns = this.config.columns;
+
+        for (let i = rows - 1; i >= 0; i--) {
+            let isRowEmpty = true;
+            for (let j = 0; j < columns; j++) {
+                if (this.map[i][j] !== '') {
+                    isRowEmpty = false;
+                    Tetromino.prototype.drawBlock.call(this, this.map[i][j], size * j, size * i);
+                }
+            }
+
+            // empty row, there won't be any blocks above
+            if (isRowEmpty) {
+                break;
+            }
+        }
+    }
+
+    canTetrominoTransform(tetromino) {
+        if (tetromino === undefined) {
+            return false;
+        }
+
+        const nextShape = tetromino.getNextShape();
+        this.tetrominoPositions = tetromino.getShapePosition(nextShape);
+
+        return this._isTetrominoPositionLegal();
+    }
+
+    canTetrominoMove(tetromino, offsetX, offsetY) {
+        if (tetromino === undefined) {
+            return false;
+        }
+
+        offsetX = offsetX || 0;
+        offsetY = offsetY || 0;
+
+        this.tetrominoPositions = tetromino.getShapePosition(null, offsetX, offsetY);
+
+        return this._isTetrominoPositionLegal();
+    }
+
+    reachBottom(tetromino) {
+        if (tetromino === undefined) {
+            return false;
+        }
+
+        const positions = tetromino.getShapePosition();
+
+        for (let i = 0; i < positions.length; i++) {
+            const x = positions[i][0];
+            const y = positions[i][1];
+            if (y + 1 === this.config.rows || this.map[y + 1][x] !== '') {
+                // reach bottom || there is a color block below
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    setTetrominoToMap(tetromino) {
+        if (tetromino === undefined) {
+            return false;
+        }
+
+        const positions = tetromino.getShapePosition();
+        const color = tetromino.getColor();
+
+        for (let i = 0; i < positions.length; i++) {
+            const x = positions[i][0];
+            const y = positions[i][1];
+            this.map[y][x] = color;
+        }
+    }
+
+    _isTetrominoPositionLegal() {
+        const positions = this.tetrominoPositions;
+
+        for (let i = 0; i < positions.length; i++) {
+            const x = positions[i][0];
+            const y = positions[i][1];
+            if (x < this.config.columns && x >= 0 && y < this.config.rows && y >= 0) {
+                if (this.map[y][x] !== '') {
+                    // already had a block here
+                    return false;
+                }
+            } else {
+                // out of map area
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- javascripts/tetrismap.js
+++ javascripts/tetrismap.js
@@ -38,17 +38,17 @@ define(['exports', 'javascripts/tetromino'], function (exports, _tetromino) {
     }();
 
     var TetrisMap = function () {
-        function TetrisMap(tetrominoSize, ctx) {
+        function TetrisMap(config, ctx) {
             _classCallCheck(this, TetrisMap);
 
-            this.tetrominoSize = tetrominoSize;
+            this.config = config;
             this.ctx = ctx;
 
             // init map
             this.map = [];
-            for (var i = 0; i < 20; i++) {
+            for (var i = 0; i < this.config.rows; i++) {
                 var row = [];
-                for (var j = 0; j < 10; j++) {
+                for (var j = 0; j < this.config.columns; j++) {
                     row.push('');
                 }
                 this.map.push(row);
@@ -56,25 +56,132 @@ define(['exports', 'javascripts/tetromino'], function (exports, _tetromino) {
         }
 
         _createClass(TetrisMap, [{
-            key: '_drawBackground',
-            value: function _drawBackground() {
+            key: 'drawBackground',
+            value: function drawBackground() {
                 var ctx = this.ctx;
-                var size = this.tetrominoSize;
+                var size = this.config.tetrominoSize;
+                var rows = this.config.rows;
+                var columns = this.config.columns;
 
                 // draw a gradient background color
-                var lineGradient = this.ctx.createLinearGradient(0, 0, size * 10, size * 20);
-                lineGradient.addColorStop(0, '#040a1b');
-                lineGradient.addColorStop(1, '#082877');
+                var lineGradient = ctx.createLinearGradient(0, 0, size * columns, size * rows);
+                lineGradient.addColorStop(0, '#082877');
+                lineGradient.addColorStop(1, '#040a1b');
                 ctx.fillStyle = lineGradient;
-                ctx.fillRect(0, 0, size * 10, size * 20);
+                ctx.fillRect(0, 0, size * columns, size * rows);
 
                 // draw a white grid
                 ctx.strokeStyle = 'white';
-                for (var i = 0; i < 10; i++) {
-                    for (var j = 0; j < 20; j++) {
+                for (var i = 0; i < columns; i++) {
+                    for (var j = 0; j < rows; j++) {
                         ctx.strokeRect(size * i, size * j, size, size);
                     }
                 }
+            }
+        }, {
+            key: 'draw',
+            value: function draw() {
+                var size = this.config.tetrominoSize;
+                var rows = this.config.rows;
+                var columns = this.config.columns;
+
+                for (var i = rows - 1; i >= 0; i--) {
+                    var isRowEmpty = true;
+                    for (var j = 0; j < columns; j++) {
+                        if (this.map[i][j] !== '') {
+                            isRowEmpty = false;
+                            _tetromino2.default.prototype.drawBlock.call(this, this.map[i][j], size * j, size * i);
+                        }
+                    }
+
+                    // empty row, there won't be any blocks above
+                    if (isRowEmpty) {
+                        break;
+                    }
+                }
+            }
+        }, {
+            key: 'canTetrominoTransform',
+            value: function canTetrominoTransform(tetromino) {
+                if (tetromino === undefined) {
+                    return false;
+                }
+
+                var nextShape = tetromino.getNextShape();
+                this.tetrominoPositions = tetromino.getShapePosition(nextShape);
+
+                return this._isTetrominoPositionLegal();
+            }
+        }, {
+            key: 'canTetrominoMove',
+            value: function canTetrominoMove(tetromino, offsetX, offsetY) {
+                if (tetromino === undefined) {
+                    return false;
+                }
+
+                offsetX = offsetX || 0;
+                offsetY = offsetY || 0;
+
+                this.tetrominoPositions = tetromino.getShapePosition(null, offsetX, offsetY);
+
+                return this._isTetrominoPositionLegal();
+            }
+        }, {
+            key: 'reachBottom',
+            value: function reachBottom(tetromino) {
+                if (tetromino === undefined) {
+                    return false;
+                }
+
+                var positions = tetromino.getShapePosition();
+
+                for (var i = 0; i < positions.length; i++) {
+                    var x = positions[i][0];
+                    var y = positions[i][1];
+                    if (y + 1 === this.config.rows || this.map[y + 1][x] !== '') {
+                        // reach bottom || there is a color block below
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }, {
+            key: 'setTetrominoToMap',
+            value: function setTetrominoToMap(tetromino) {
+                if (tetromino === undefined) {
+                    return false;
+                }
+
+                var positions = tetromino.getShapePosition();
+                var color = tetromino.getColor();
+
+                for (var i = 0; i < positions.length; i++) {
+                    var x = positions[i][0];
+                    var y = positions[i][1];
+                    this.map[y][x] = color;
+                }
+            }
+        }, {
+            key: '_isTetrominoPositionLegal',
+            value: function _isTetrominoPositionLegal() {
+                var positions = this.tetrominoPositions;
+
+                for (var i = 0; i < positions.length; i++) {
+                    var x = positions[i][0];
+                    var y = positions[i][1];
+                    if (x < this.config.columns && x >= 0 && y < this.config.rows && y >= 0) {
+                        if (this.map[y][x] !== '') {
+                            // already had a block here
+                            return false;
+                        }
+                    } else {
+                        // out of map area
+                        return false;
+                    }
+                }
+
+                return true;
             }
         }]);
 
